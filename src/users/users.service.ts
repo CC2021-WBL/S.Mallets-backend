7405ddbd16c395bf366198665b971d1d96af6c7c
Monika Krella <monika.krella@gmail.com>
2022-05-06 20:41:26 +0200

    smallfix

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -26,7 +26,6 @@ export class UsersService {
   async findByEmail(email: string) {
     const user = await this.usersRepository.findOne({ email });
     if (user) {
-      user.hash = undefined;
       return user;
     }
     throw new HttpException(
