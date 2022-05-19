e7a91ba03e006b28d79a099d2edac9074cf074d9
Monika Krella <monika.krella@gmail.com>
2022-05-19 20:44:04 +0200

    test5

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,16 @@ const whitelist = [
 ];
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
   });
-  app.enableCors();
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
+  // app.enableCors();
   // app.enableCors({
   //   origin: function (origin, callback) {
   //     if (!origin || whitelist.indexOf(origin) !== -1) {
